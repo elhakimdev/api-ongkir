235d1b4eca9d44cb4d76dc760a7c0cbd4ce2231f
Elhakim Dev <abdullehakim68@gmail.com>
2021-11-18 22:05:02 +0700

    change key on env config provider_api_key

--- src/ShipmentServiceProvider.php
+++ src/ShipmentServiceProvider.php
@@ -10,7 +10,7 @@ class ShipmentServiceProvider extends ServiceProvider
        public function register()
        {
               $this->app->bind('Shipment', function(){
-                     return new Shipment(env('API_KEY'));
+                     return new Shipment(env('PROVIDER_API_KEY'));
               });
        }
 }
