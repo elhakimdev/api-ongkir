197fa03986d97d707f10a1cdb67381227b343e94
Elhakim Dev <abdullehakim68@gmail.com>
2021-11-18 22:01:02 +0700

    remove env loader on package dev

--- src/ShipmentServiceProvider.php
+++ src/ShipmentServiceProvider.php
@@ -9,10 +9,6 @@ class ShipmentServiceProvider extends ServiceProvider
 {
        public function register()
        {
-              // $this->app->singleton(Shipment::class, function () {
-              //        return new Shipment(env('API_KEY'));
-              // });
-              (Dotenv::createImmutable(__DIR__ . '../../'))->load();
               $this->app->bind('Shipment', function(){
                      return new Shipment(env('API_KEY'));
               });
